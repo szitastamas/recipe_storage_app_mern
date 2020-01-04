c168d0d210fc7fd568253b8de275e75695e212a6
Szitás Tamás <szitastamas@hotmail.com>
2020-01-04 16:36:42 +0100

    Express Server Setup

---
+++ routes/recipes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/recipes
+// @desc    Get all user's recipes
+// @access  Private
+router.get('/', (req, res) => {
+    res.send('Get own recipes...');
+})
+
+router.get('/:id', (req, res)=>{
+    res.send('Getting one recipe...')
+})
+
+// @route   POST api/recipes
+// @desc    Add a new recipe
+// @access  Private
+router.post('/', (req, res) => {
+    res.send('Add recipe');
+})
+
+// @route   PUT api/recipes/:id
+// @desc    Update Recipe
+// @access  Private
+router.put('/:id', (req, res) => {
+    res.send('Update recipe');
+})
+
+// @route   DELETE api/recipes/:id
+// @desc    Delete Recipe
+// @access  Private
+router.delete('/:id', (req, res) => {
+    res.send('Delete recipe');
+})
+
+module.exports = router;
