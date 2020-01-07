f7b3f7ee88e87cfbe1f1e7ee4cc166cfcab70002
szitastamas <szitastamas@hotmail.com>
2020-01-07 08:46:17 +0100

    Updated Loading Animation

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,27 +1,28 @@
-import React, { useEffect, useContext } from "react";
-import Recipes from "../recipes/Recipes";
-import AuthContext from "../../contexts/auth/AuthContext";
+import React, { useEffect, useContext } from 'react';
+import Recipes from '../recipes/Recipes';
+import AuthContext from '../../contexts/auth/AuthContext';
+import AlertContext from '../../contexts/alert/AlertContext';
+const Dashboard = props => {
+    const authContext = useContext(AuthContext);
+    const { isAuthenticated, loadUser } = authContext;
+    const alertContext = useContext(AlertContext);
 
-const Dashboard = (props) => {
+    useEffect(() => {
+        if (!isAuthenticated) {
+            props.history.push('/');
+            alertContext.setAlert('Log in to see your dashboard', 'fail');
+        }
+        loadUser();
+        // eslint-disable-next-line
+    }, []);
 
-  const authContext = useContext(AuthContext);
-  const { isAuthenticated, loadUser } = authContext;
-
-  useEffect(() => {
-    if(!isAuthenticated){
-      props.history.push("/");
-    }
-    loadUser();
-    // eslint-disable-next-line
-  }, [])
-
-  return (
-    <div className="col s12 l6">
-      <h1 className="flow-text">Your personal Backend</h1>
-      <h4 className="flow-text">Update or delete your recipes here</h4>
-      <Recipes customClassName="dashboard-recipe-grid" location="dashboard"/>
-    </div>
-  );
+    return (
+        <div className='col s12 l6'>
+            <h1 className='flow-text'>Your personal Backend</h1>
+            <h4 className='flow-text'>Update or delete your recipes here</h4>
+            <Recipes customClassName='dashboard-recipe-grid' location='dashboard' />
+        </div>
+    );
 };
 
 export default Dashboard;
--- client/src/components/utlity/LoadingDiv.js
+++ client/src/components/utlity/LoadingDiv.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const LoadingDiv = () => {
     return (
-        <div id="loading-div">
-            <div className="loading-dot"></div>
-            <div className="loading-dot"></div>
-            <div className="loading-dot"></div>
+        <div id='loading-div'>
+            <div className='loading-container'>
+                <div className='loading-dot'></div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default LoadingDiv;
