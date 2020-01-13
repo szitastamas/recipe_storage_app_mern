8c6d9db0894619dc6bb10849cc9f8684a2f6f1fa
szitastamas <szitastamas@hotmail.com>
2020-01-13 14:55:59 +0100

    Process on theming - Dashboard1

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,139 +1,121 @@
-import React, { useEffect, useContext, useState } from "react";
-import { Link } from "react-router-dom";
-import AuthContext from "../../contexts/auth/AuthContext";
-import RecipeContext from "../../contexts/recipe/RecipeContext";
-import RecipeForm from "../forms/RecipeForm";
-import DashboardInfo from "./dashboard/DashboardInfo";
-import LoadingDiv from "../utlity/LoadingDiv";
+import React, { useEffect, useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import AuthContext from '../../contexts/auth/AuthContext';
+import RecipeContext from '../../contexts/recipe/RecipeContext';
+import RecipeForm from '../forms/RecipeForm';
+import DashboardInfo from './dashboard/DashboardInfo';
+import LoadingDiv from '../utlity/LoadingDiv';
 import RecipeDetails from '../recipes/RecipeDetails';
+import { LoadTheme } from '../theme/LoadTheme';
 
 // import AlertContext from "../../contexts/alert/AlertContext";
 const Dashboard = () => {
-  const authContext = useContext(AuthContext);
-  const { isAuthenticated, loadUser } = authContext;
+    const authContext = useContext(AuthContext);
+    const { isAuthenticated, loadUser } = authContext;
 
-  const recipeContext = useContext(RecipeContext);
-  const {
-    getOwnRecipes,
-    ownRecipes,
-    loading,
-    triggerLoading,
-    deleteRecipe
-  } = recipeContext;
+    const recipeContext = useContext(RecipeContext);
+    const { getOwnRecipes, ownRecipes, loading, triggerLoading, deleteRecipe } = recipeContext;
 
-  useEffect(() => {
-    triggerLoading();
-    loadUser();
-    getOwnRecipes();
+    useEffect(() => {
+        triggerLoading();
+        loadUser();
+        getOwnRecipes();
 
-    // eslint-disable-next-line
-  }, [isAuthenticated]);
+        // eslint-disable-next-line
+    }, [isAuthenticated]);
 
-  const [recipeToEdit, setRecipeToEdit] = useState(null);
+    const [recipeToEdit, setRecipeToEdit] = useState(null);
 
-  const handleEdit = recipe => {
-    setRecipeToEdit(recipe);
-  };
+    const handleEdit = recipe => {
+        setRecipeToEdit(recipe);
+    };
 
-  const [recipeDetails, setRecipeDetails] = useState(null);
+    const [recipeDetails, setRecipeDetails] = useState(null);
 
-  const showDetails = recipe => {
-    setRecipeDetails(recipe);
-  };
+    const showDetails = recipe => {
+        setRecipeDetails(recipe);
+    };
 
-  const removeDetails = () => {
-    setRecipeDetails(null);
-  };
+    const removeDetails = () => {
+        setRecipeDetails(null);
+    };
 
-  const cancel = () => {
-    setRecipeToEdit(null);
-  };
+    const cancel = () => {
+        setRecipeToEdit(null);
+    };
 
-  return (
-    <div className="col s12 l6">
-      <h1 className="flow-text teal-text">Your personal Recipe Storage</h1>
-      <div className="row">
-        <div className="col s12 m6">
-          <DashboardInfo />
-        </div>
-        <div className="col s12 m6">
-          <div className="container">
-            <RecipeForm recipeToEdit={recipeToEdit} cancel={cancel} />
-          </div>
-        </div>
-      </div>
-      {loading ? (
-        <LoadingDiv loaderType="spinner" />
-      ) : ownRecipes.length > 0 ? (
-        // <Recipes customGrid='dashboard-recipe-grid' title='Check all your own recipes' recipes={ownRecipes} />
-        <ul className="collection">
-          <div className="collection-item" key={"dashboard-list-header"}>
-            <div className="row">
-              <div
-                className="col s6 teal-text"
-                style={{ fontWeight: "600", textTransform: "uppercase" }}
-              >
-                Title
-              </div>
-              <div className="col s3" style={{ fontWeight: "600" }}>
-                Type
-              </div>
-              <div className="col s3"></div>
-            </div>
-          </div>
-          {ownRecipes.map(recipe => {
-            return (
-              <li key={recipe._id} className="collection-item">
-                <div className="row">
-                  <div
-                    className="col s6 teal-text"
-                    style={{
-                      fontWeight: "600",
-                      textTransform: "uppercase",
-                      cursor: "pointer"
-                    }}
-                    onClick={() => showDetails(recipe)}
-                  >
-                    {recipe.title}
-                  </div>
-                  <div
-                    className="col s3"
-                    style={{ textTransform: "capitalize" }}
-                  >
-                    {recipe.type}
-                  </div>
-                  <div className="col s3 right">
-                    <Link
-                      to="#"
-                      className="secondary-content btn small red"
-                      onClick={() => deleteRecipe(recipe._id)}
-                    >
-                      <i className="material-icons">delete</i>
-                    </Link>
-                    <Link
-                      to="#"
-                      className="secondary-content btn blue lighten-2"
-                      onClick={() => handleEdit(recipe)}
-                    >
-                      <i className="material-icons">edit</i>
-                    </Link>
-                  </div>
+    const activeTheme = LoadTheme();
+
+    const { uiColor, bgColor, mainTextColor, secondaryTextColor } = activeTheme;
+
+    return (
+        <div className='col s12 l6'>
+            <h1 className='flow-text teal-text'>Your personal Recipe Storage</h1>
+            <div className='row'>
+                <div className='col s12 m6'>
+                    <DashboardInfo />
                 </div>
-              </li>
-            );
-          })}
-        </ul>
-      ) : (
-        <p className="center" style={{ padding: "2rem" }}>
-          Your Recipe Storage is empty. Upload some of your favourite recipes
-          today!
-        </p>
-      )}
-      {recipeDetails && (
-        <RecipeDetails recipe={recipeDetails} removeDetails={removeDetails} />
-      )}
-    </div>
-  );
+                <div className='col s12 m6'>
+                    <div className='container'>
+                        <RecipeForm recipeToEdit={recipeToEdit} cancel={cancel} />
+                    </div>
+                </div>
+            </div>
+            {loading ? (
+                <LoadingDiv loaderType='spinner' />
+            ) : ownRecipes.length > 0 ? (
+                // <Recipes customGrid='dashboard-recipe-grid' title='Check all your own recipes' recipes={ownRecipes} />
+                <ul className='collection'>
+                    <div className='collection-item' key={'dashboard-list-header'}>
+                        <div className='row'>
+                            <div className='col s6 teal-text' style={{ fontWeight: '600', textTransform: 'uppercase' }}>
+                                Title
+                            </div>
+                            <div className={`col s3 ${secondaryTextColor}`} style={{ fontWeight: '600' }}>
+                                Type
+                            </div>
+                            <div className={`col s3 ${secondaryTextColor}`}></div>
+                        </div>
+                    </div>
+                    {ownRecipes.map(recipe => {
+                        return (
+                            <li key={recipe._id} className='collection-item'>
+                                <div className='row'>
+                                    <div
+                                        className='col s6 teal-text'
+                                        style={{
+                                            fontWeight: '600',
+                                            textTransform: 'uppercase',
+                                            cursor: 'pointer'
+                                        }}
+                                        onClick={() => showDetails(recipe)}
+                                    >
+                                        {recipe.title}
+                                    </div>
+                                    <div className={`col s3 ${secondaryTextColor}`} style={{ textTransform: 'capitalize' }}>
+                                        {recipe.type}
+                                    </div>
+                                    <div className={`col s3 right`}>
+                                        <Link to='#' className='secondary-content btn small red' onClick={() => deleteRecipe(recipe._id)}>
+                                            <i className='material-icons'>delete</i>
+                                        </Link>
+                                        <Link to='#' className='secondary-content btn blue lighten-2' onClick={() => handleEdit(recipe)}>
+                                            <i className='material-icons'>edit</i>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
+            ) : (
+                <p className={`center ${secondaryTextColor}`} style={{ padding: '2rem' }}>
+                    Your Recipe Storage is empty. Upload some of your favourite recipes today!
+                </p>
+            )}
+            {recipeDetails && <RecipeDetails recipe={recipeDetails} removeDetails={removeDetails} />}
+        </div>
+    );
 };
 
 export default Dashboard;
--- client/src/components/recipes/RecipeDetails.js
+++ client/src/components/recipes/RecipeDetails.js
@@ -1,53 +1,58 @@
 import React, { useEffect } from 'react';
+import { LoadTheme } from '../theme/LoadTheme';
 
 const RecipeDetails = ({ recipe, removeDetails }) => {
-	const { title, description, type, privacy, date, picture } = recipe;
+    const { title, description, type, privacy, date, picture } = recipe;
 
-	useEffect(() => {
-		document.querySelector('.overlay').addEventListener('click', (e) => {
-			if (e.target === e.currentTarget) {
-				removeDetails();
-			}
-		});
+    useEffect(() => {
+        document.querySelector('.overlay').addEventListener('click', e => {
+            if (e.target === e.currentTarget) {
+                removeDetails();
+            }
+        });
 
-		window.addEventListener('keydown', (e) => {
-			if (e.key === 'Escape') {
-				removeDetails();
-			}
-		});
-	});
+        window.addEventListener('keydown', e => {
+            if (e.key === 'Escape') {
+                removeDetails();
+            }
+        });
+    });
 
-	return (
-		<div id='recipe-detail-modal'>
-			<div className='overlay'>
-				<div className='card horizontal' id='recipe-details-card'>
-					<div className='card-stacked'>
-						<div className='card-content'>
-							<h4 className='teal-text'>{title}</h4>
-							<hr />
-							<p style={{ padding: '2rem 0' }}>{description}</p>
-						</div>
-						<div className='card-action'>
-							<div className='row'>
-								<div className='col s4'>
-									<strong>Type of dish: </strong>
-									{type}
-								</div>
-								<div className='col s4'>
-									<strong>Privacy settings: </strong>
-									{privacy}
-								</div>
-								<div className='col s4'>
-									<strong>Created on: </strong>
-									{date.slice(0, 10)}
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    const activeTheme = LoadTheme();
+
+    const { uiColor, bgColor, mainTextColor, secondaryTextColor } = activeTheme;
+
+    return (
+        <div id='recipe-detail-modal'>
+            <div className='overlay'>
+                <div className={`card horizontal ${uiColor} ${secondaryTextColor}`} id='recipe-details-card'>
+                    <div className='card-stacked'>
+                        <div className='card-content'>
+                            <h4 className='teal-text'>{title}</h4>
+                            <hr />
+                            <p style={{ padding: '2rem 0' }}>{description}</p>
+                        </div>
+                        <div className='card-action'>
+                            <div className='row'>
+                                <div className='col s4'>
+                                    <strong>Type of dish: </strong>
+                                    {type}
+                                </div>
+                                <div className='col s4'>
+                                    <strong>Privacy settings: </strong>
+                                    {privacy}
+                                </div>
+                                <div className='col s4'>
+                                    <strong>Created on: </strong>
+                                    {date.slice(0, 10)}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default RecipeDetails;
